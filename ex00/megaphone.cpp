19db99c910ab4511ce1c5ffdaa8244c6a38d71a3
Julia Ortiz Mora <jortiz-m@c3r3s1.42malaga.com>
2025-06-30 13:24:37 +0200

    megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	if(ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i++;
+		while(i < ac)
+		{
+			j = 0;
+			while(av[i][j])
+			{
+				std::cout << static_cast<char>(std::toupper(av[i][j]));
+                ++j;
+			}
+			std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return(0);
+}
